20bbd6ed85bf8f975072db28f023a229823a3378
Admin <146722351+programm-admin@users.noreply.github.com>
2025-01-24 14:54:26 +0100

    update styles for login component

--- Frontend/src/features/start/login/login.component.ts
+++ Frontend/src/features/start/login/login.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { FloatLabel } from 'primeng/floatlabel';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-login',
-  imports: [FloatLabel, InputTextModule, PasswordModule],
+  imports: [FloatLabel, InputTextModule, PasswordModule, ButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
