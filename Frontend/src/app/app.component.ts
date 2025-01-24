c4559910337bb31596000a177f10519f1c523471
Admin <146722351+programm-admin@users.noreply.github.com>
2025-01-24 20:45:13 +0100

    update css + routes

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { InputTextModule } from 'primeng/inputtext';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
-import { HeaderMainComponent } from '../features/header/header-main/header-main.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, InputTextModule, InputIconModule, IconFieldModule, HeaderMainComponent],
+  imports: [RouterOutlet, InputTextModule, InputIconModule, IconFieldModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Frontend/src/features/start/start-main/start-main.component.ts
+++ Frontend/src/features/start/start-main/start-main.component.ts
@@ -3,16 +3,29 @@ import { InputTextModule } from 'primeng/inputtext';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { TooltipModule } from 'primeng/tooltip';
-import { HeaderMainComponent } from '../../header/header-main/header-main.component';
-import { RouterOutlet } from '@angular/router';
-
+import { ButtonModule } from 'primeng/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start-main',
-  imports: [InputTextModule, IconFieldModule, InputIconModule, TooltipModule, HeaderMainComponent, RouterOutlet],
+  imports: [
+    InputTextModule,
+    IconFieldModule,
+    InputIconModule,
+    TooltipModule,
+    ButtonModule,
+  ],
   templateUrl: './start-main.component.html',
-  styleUrl: './start-main.component.css'
+  styleUrl: './start-main.component.css',
 })
 export class StartMainComponent {
+  constructor(private router: Router) {}
 
+  navigateToLogin = () => {
+    this.router.navigateByUrl('/login');
+  };
+
+  navigateToRegister = () => {
+    this.router.navigateByUrl('/register');
+  };
 }
