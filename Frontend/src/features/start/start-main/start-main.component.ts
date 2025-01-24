ef7a2b8a0c0f3ae0e600a72e3fbfda98ec8d64c6
Admin <146722351+programm-admin@users.noreply.github.com>
2025-01-24 12:00:21 +0100

    create start module with start component

---
+++ Frontend/src/features/start/start-main/start-main.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-main',
+  imports: [],
+  templateUrl: './start-main.component.html',
+  styleUrl: './start-main.component.css'
+})
+export class StartMainComponent {
+
+}
