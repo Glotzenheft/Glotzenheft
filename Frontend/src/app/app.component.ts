4a10f2f0c6b040adaf53bb7d7dd7cf00bf1e1040
Admin <146722351+programm-admin@users.noreply.github.com>
2025-01-25 14:30:42 +0100

    CREATE toggeling sidebar

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -4,6 +4,9 @@ import { InputTextModule } from 'primeng/inputtext';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { FooterComponent } from '../features/components/footer/footer.component';
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-root',
@@ -13,8 +16,17 @@ import { FooterComponent } from '../features/components/footer/footer.component'
     InputIconModule,
     IconFieldModule,
     FooterComponent,
+    ButtonModule,
+    TooltipModule,
+    CommonModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  isSidebarOpen: boolean = true; // sidebar should be open by default
+
+  toggleSidebar = (newValue: boolean) => {
+    this.isSidebarOpen = newValue;
+  };
+}
