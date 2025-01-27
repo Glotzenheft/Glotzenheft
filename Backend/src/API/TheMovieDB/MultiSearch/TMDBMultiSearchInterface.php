d848e678bf9d19720b98c6dace4b07f9cc9a619b
Enuresh <138758218+Enuresh@users.noreply.github.com>
2025-01-27 01:37:19 +0100

    Added TMDBMultiSearchController.php and TMDBMultiSearchService.php. Added multi search route for API requests. Added TMDB_MULTI_SEARCH_URL to .env file.

--- Backend/src/API/TheMovieDB/MultiSearch/TMDBMultiSearchInterface.php
+++ Backend/src/API/TheMovieDB/MultiSearch/TMDBMultiSearchInterface.php
@@ -4,5 +4,5 @@ namespace App\API\TheMovieDB\MultiSearch;
 
 interface TMDBMultiSearchInterface
 {
-
+    public function multiSearch(string $q, int $page = 1): array;
 }
---
+++ Backend/src/API/TheMovieDB/MultiSearch/TMDBMultiSearchService.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\API\TheMovieDB\MultiSearch;
+
+use Psr\Log\LoggerInterface;
+use RuntimeException;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+readonly class TMDBMultiSearchService implements TMDBMultiSearchInterface
+{
+    public function __construct
+    (
+        #[Autowire('%env(TMDB_TOKEN)%')]
+        private string $tmdbToken,
+        #[Autowire('%env(TMDB_MULTI_SEARCH_URL)%')]
+        private string $multiSearchUrl,
+        private HttpClientInterface $httpClient,
+        private LoggerInterface $logger
+    )
+    {
+    }
+
+    public function multiSearch(string $q, int $page = 1): array
+    {
+        try
+        {
+            $response = $this->httpClient->request('GET', $this->multiSearchUrl, [
+                'query' => [
+                    'query' => $q,
+                    'page' => $page,
+                ],
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->tmdbToken,
+                ],
+            ]);
+
+            $decodedResponse = json_decode($response->getContent(), true);
+
+            if (!isset($decodedResponse['results']))
+            {
+                throw new RuntimeException('Unerwartetes API-Antwortformat.');
+            }
+
+            return $decodedResponse;
+        }
+        catch (TransportExceptionInterface|ClientExceptionInterface|RedirectionExceptionInterface|ServerExceptionInterface $e)
+        {
+            $this->logger->error('Exception occured while multi-search service: ' . $e->getMessage());
+            return [];
+        }
+    }
+}
