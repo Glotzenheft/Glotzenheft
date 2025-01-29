fddadcdbbaa3ebfe100b0cfbf9fed76b89bd228d
Enuresh <138758218+Enuresh@users.noreply.github.com>
2025-01-30 00:59:02 +0100

    Added TMDB API integration in TMDBTVSeriesDetailsService.php. Added API endpoint to retrieve tv series information through TVSeriesDetailsController.

---
+++ Backend/src/Controller/API/Media/TVSeriesDetailsController.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\API\Media;
+
+use App\API\TheMovieDB\Traits\MediaDetailTrait;
+use App\Enum\MediaType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+
+class TVSeriesDetailsController extends AbstractController
+{
+    use MediaDetailTrait;
+
+    /**
+     * @example https://127.0.0.1:8000/api/tv?tmdbID=205366
+     * @param Request $request
+     * @return JsonResponse
+     */
+    #[Route('/api/tv')]
+    public function getTVSeriesDetails(Request $request): JsonResponse
+    {
+        $tmdbID = (int)$request->query->get('tmdbID');
+
+        if (empty($tmdbID))
+        {
+            return $this->json(['error' => 'Query parameter "tmdbID" is required.'], 400);
+        }
+
+        $media = $this->handleTMDBMediaDetail($tmdbID, MediaType::TV);
+
+        return $this->json($media, context: ['groups' => ['media_details']]);
+    }
+}
