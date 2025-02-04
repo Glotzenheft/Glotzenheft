2cda9759724677ec164aa8c850eb67156f9cd325
Enuresh <138758218+Enuresh@users.noreply.github.com>
2025-02-04 13:35:44 +0100

    Added movie details service and api endpoint. Updated MediaDetailTrait.php for movie detail service.

--- Backend/src/API/TheMovieDB/Movies/Details/TMDBMovieDetailsService.php
+++ Backend/src/API/TheMovieDB/Movies/Details/TMDBMovieDetailsService.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace App\API\TheMovieDB\TVSeries\Details;
+namespace App\API\TheMovieDB\Movies\Details;
 
 use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
@@ -10,14 +10,13 @@ use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
-readonly class TMDBTVSeriesDetailsService implements TMDBTVSeriesDetailsInterface
+readonly class TMDBMovieDetailsService implements TMDBMovieDetailsInterface
 {
-
     public function __construct
     (
         #[Autowire('%env(TMDB_TOKEN)%')]
         private string $tmdbToken,
-        #[Autowire('%env(TMDB_TV_SERIES_DETAILS_URL)%')]
+        #[Autowire('%env(TMDB_MOVIE_DETAILS_URL)%')]
         private string $apiUrl,
         private HttpClientInterface $httpClient,
         private LoggerInterface $logger
@@ -25,11 +24,11 @@ readonly class TMDBTVSeriesDetailsService implements TMDBTVSeriesDetailsInterfac
     {
     }
 
-    public function getTVSeriesDetails(int $seriesID, string $language = 'de-DE'): array
+    public function getMovieDetails(int $movieID, string $language = 'de-DE'): array
     {
         try
         {
-            $response = $this->httpClient->request('GET', $this->apiUrl . $seriesID . '?append_to_response=external_ids&language=' . $language, [
+            $response = $this->httpClient->request('GET', $this->apiUrl . $movieID . '?append_to_response=external_ids&language=' . $language, [
                 'headers' => [
                     'Authorization' => 'Bearer ' . $this->tmdbToken,
                     'accept' => 'application/json',
@@ -40,7 +39,7 @@ readonly class TMDBTVSeriesDetailsService implements TMDBTVSeriesDetailsInterfac
         }
         catch (TransportExceptionInterface|ClientExceptionInterface|RedirectionExceptionInterface|ServerExceptionInterface $e)
         {
-            $this->logger->error('Exception occured while getTVSeriesDetails: ' . $e->getMessage());
+            $this->logger->error('Exception occured while TMDBMovieDetailsService: ' . $e->getMessage());
             return [];
         }
     }
