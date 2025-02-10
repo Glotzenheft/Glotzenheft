862eca3b4c49cf097b008ab2b6d4da9a93b8c4a4
Admin <146722351+programm-admin@users.noreply.github.com>
2025-02-10 10:33:22 +0100

    UPDATE api routes with tv season details

--- Frontend/src/shared/variables/api-routes.ts
+++ Frontend/src/shared/variables/api-routes.ts
@@ -4,4 +4,5 @@ export const ROUTE_MULTI_SEARCH: string =
 export const ROUTE_MEDIA_DETAILS_SEARCH: string =
   'https://127.0.0.1:8000/api/tv?tmdbID=';
 
-  
+export const ROUTE_SEASON_DETAILS: string =
+  'https://127.0.0.1:8000/api/tv/season';
