5ebcb9b70ab8748c11efbb893d907c38bb7794ea
Admin <146722351+programm-admin@users.noreply.github.com>
2025-02-14 12:12:52 +0100

    UPDATE api routes

--- Frontend/src/shared/variables/api-routes.ts
+++ Frontend/src/shared/variables/api-routes.ts
@@ -4,8 +4,12 @@ export const ROUTE_MULTI_SEARCH: string =
 export const ROUTE_MEDIA_DETAILS_SEARCH: string =
   'https://127.0.0.1:8000/api/tv?tmdb_id=';
 
+//127.0.0.1:8000/api/tv?media_id=1&tmdb_id=205366
+
 export const ROUTE_MOVIE_DETAILS_SEARCH: string =
   'https://127.0.0.1:8000/api/movie?tmdb_id=';
+
+// https://127.0.0.1:8000/api/tv?media_id=1&tmdb_id=205366
 
 export const ROUTE_SEASON_DETAILS: string =
   'https://127.0.0.1:8000/api/tv/season';
