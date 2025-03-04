38d0b8f14262cb151ed1b8a6884773ac9cae83ea
Enuresh <138758218+Enuresh@users.noreply.github.com>
2025-03-04 10:08:38 +0100

    Fix DeleteTest.php.

--- Backend/tests/Integration/DeleteTest.php
+++ Backend/tests/Integration/DeleteTest.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\User;
 use App\Enum\SecurityQuestions;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class DeleteTest extends WebTestCase
 {
@@ -15,24 +16,42 @@ class DeleteTest extends WebTestCase
     private $client;
     private String $token;
 
+    private string $securityAnswer;
+
     protected function setUp(): void
     {
         $this->client = static::createClient();
         $this->entityManager = static::getContainer()->get(EntityManagerInterface::class);
+        $passwordHasher = static::getContainer()->get(UserPasswordHasherInterface::class);
 
         // Create test user object
-        $this->user = new User();
-        $this->user->setUsername('testuser5');
-        $this->user->setPassword('123456767!'); //123456677
-        $this->user->setSecurityQuestion(SecurityQuestions::PET_COLOR);
-        $this->user->setSecurityAnswer('Grey');
+        $username = 'testuser';
+        $this->securityAnswer = 'Grey';
+        $user = $this->entityManager->getRepository(User::class)->findOneBy(['username' => $username]);
+
+        if (!$user instanceof User)
+        {
+            $this->user = new User();
+            $this->user->setUsername($username);
+            $hashedPassword = $passwordHasher->hashPassword($this->user, '123456677!');
+            $this->user->setPassword($hashedPassword);
+            $this->user->setSecurityQuestion(SecurityQuestions::PET_COLOR);
+            $hashedSecurityAnswer = $passwordHasher->hashPassword($this->user, $this->securityAnswer);
+            $this->user->setSecurityAnswer($hashedSecurityAnswer);
+            $this->entityManager->persist($this->user);
+            $this->entityManager->flush();
+        }
+        else
+        {
+            $this->user = $user;
+        }
     }
     public function testUserDelete(): void
     {
         // Log in to get JWT token
         $loginData = [
             'username' => $this->user->getUsername(),
-            'password' => $this->user->getPassword(),
+            'password' => '123456677!',
         ];
         $this->client->request('POST', '/api/login', [], [], ['CONTENT_TYPE' => 'application/json'], json_encode($loginData));
         $responseData = json_decode($this->client->getResponse()->getContent(), true);
@@ -40,7 +59,7 @@ class DeleteTest extends WebTestCase
 
         $deleteData = [
             'security_question' => $this->user->getSecurityQuestion(),
-            'security_answer' => $this->user->getSecurityAnswer(),
+            'security_answer' => $this->securityAnswer,
         ];
 
         $this->client->request(
@@ -58,9 +77,11 @@ class DeleteTest extends WebTestCase
         $this->assertResponseStatusCodeSame(Response::HTTP_OK, 'User deletion failed');
         $deleteResponse = json_decode($this->client->getResponse()->getContent(), true);
         // Antwort kommt mit zwei Anführungszeichen
-        if (!is_array($deleteResponse)) {
+        if (!is_array($deleteResponse))
+        {
             $deleteResponse = ['message' => trim($this->client->getResponse()->getContent(), '"')];
         }
+        echo PHP_EOL;
         $this->assertEquals('User deleted successfully', $deleteResponse['message'] ?? '', 'Unexpected deletion message');
         echo "User deletion successful.\n";
     }
