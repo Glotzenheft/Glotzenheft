4849344641caf3b848e28cb55f36c302ccd95147
Enuresh <138758218+Enuresh@users.noreply.github.com>
2025-08-26 01:23:13 +0200

    [#6] Finish recommendation endpoints.

--- Backend/src/Controller/API/Media/MovieRecommendationController.php
+++ Backend/src/Controller/API/Media/MovieRecommendationController.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace App\Controller\API\Media;
 
 use App\Model\Request\Recommendation\MovieRecommendationDto;
+use App\Security\IsAuthenticated;
+use App\Service\TMDB\Movies\TMDBMovieRecommendationService;
+use App\TmdbApi\ApiException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Attribute\MapQueryString;
@@ -12,11 +15,30 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class MovieRecommendationController extends AbstractController
 {
-    #[Route('/api/movie-recommendation')]
-    public function movieRecommendationEndpoint(
+    public function __construct(
+        private readonly TMDBMovieRecommendationService $recommendationService,
+    ){}
+
+    /**
+     * @param MovieRecommendationDto $params
+     * @return JsonResponse
+     * @throws ApiException
+     */
+    #[IsAuthenticated]
+    #[Route('/api/movie/recommendations', name: 'get_movie_recommendations', methods: ['GET'])]
+    public function movieRecommendationsEndpoint(
         #[MapQueryString] MovieRecommendationDto $params
     ): JsonResponse
     {
-        return $this->json();
+        $response = $this->recommendationService->getMovieRecommendations($params);
+
+        $data = [
+            'page' => $response->getPage(),
+            'recommendations' => $response->getResults(),
+            'total_pages' => $response->getTotalPages(),
+            'total_results' => $response->getTotalResults(),
+        ];
+
+        return $this->json(data: $data);
     }
 }
--- Backend/src/Controller/API/Media/TVSeriesRecommendationController.php
+++ Backend/src/Controller/API/Media/TVSeriesRecommendationController.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace App\Controller\API\Media;
 
 use App\Model\Request\Recommendation\TVSeriesRecommendationDto;
+use App\Security\IsAuthenticated;
+use App\Service\TMDB\TVSeries\TMDBTVSeriesRecommendationService;
+use App\TmdbApi\ApiException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Attribute\MapQueryString;
@@ -12,11 +15,31 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class TVSeriesRecommendationController extends AbstractController
 {
-    #[Route('/api/movie-recommendation')]
-    public function movieRecommendationEndpoint(
+
+    public function __construct(
+        private readonly TMDBTVSeriesRecommendationService $recommendationService,
+    ){}
+
+    /**
+     * @param TVSeriesRecommendationDto $params
+     * @return JsonResponse
+     * @throws ApiException
+     */
+    #[IsAuthenticated]
+    #[Route('/api/tv/recommendations', name: 'get_tv_series_recommendations', methods: ['GET'])]
+    public function tvSeriesRecommendationsEndpoint(
         #[MapQueryString] TVSeriesRecommendationDto $params
     ): JsonResponse
     {
-        return $this->json();
+        $response = $this->recommendationService->getTVSeriesRecommendations($params);
+
+        $data = [
+            'page' => $response->getPage(),
+            'recommendations' => $response->getResults(),
+            'total_pages' => $response->getTotalPages(),
+            'total_results' => $response->getTotalResults(),
+        ];
+
+        return $this->json(data: $data);
     }
 }
