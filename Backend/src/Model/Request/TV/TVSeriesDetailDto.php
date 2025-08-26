9c85d9f7c98629f4ff8c5c34ecdff533f10e9df4
Enuresh <138758218+Enuresh@users.noreply.github.com>
2025-08-27 01:48:16 +0200

    [#4] Adjust movie and tv series detail DTOs.

--- Backend/src/Model/Request/TV/TVSeriesDetailDto.php
+++ Backend/src/Model/Request/TV/TVSeriesDetailDto.php
@@ -20,23 +20,55 @@ declare(strict_types=1);
 
 namespace App\Model\Request\TV;
 
+use App\Model\Request\Media\MediaDetailDtoInterface;
 use Symfony\Component\Serializer\Attribute\SerializedName;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
-class TVSeriesDetailDto
+#[Assert\Callback(callback: 'validateIds')]
+readonly class TVSeriesDetailDto implements MediaDetailDtoInterface
 {
     public function __construct(
         #[SerializedName('media_id')]
-        #[Assert\NotBlank(
-            message: 'Parameter "media_id" is required.'
-        )]
         #[Assert\Range(
             minMessage: 'Parameter "media_id" must be at least 1.',
             min: 1
         )]
         public ?int $mediaId = null,
+        #[SerializedName('tmdb_id')]
+        #[Assert\Range(
+            minMessage: 'Parameter "tmdb_id" must be at least 1.',
+            min: 1
+        )]
+        public ?int $tmdbId = null,
         public string $language = 'de-DE',
         #[SerializedName('append_to_response')]
         public ?string $appendToResponse = null,
     ){}
+
+    public function getMediaId(): ?int
+    {
+        return $this->mediaId;
+    }
+
+    public function getTmdbId(): ?int
+    {
+        return $this->tmdbId;
+    }
+
+    public function getLanguage(): string
+    {
+        return $this->language;
+    }
+
+    public function validateIds(ExecutionContextInterface $context): void
+    {
+        if ($this->mediaId === null && $this->tmdbId === null)
+        {
+            $context
+                ->buildViolation('At least one query parameter ("media_id" or "tmdb_id") is required.')
+                ->atPath('mediaId')
+                ->addViolation();
+        }
+    }
 }
