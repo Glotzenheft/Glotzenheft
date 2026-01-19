6f8e02c3be1aacef06df71879f57f4915a5bcbca
Enuresh <138758218+Enuresh@users.noreply.github.com>
2026-01-19 17:10:43 +0100

    Implement activity tabs for user activities and refactor activities page components. Added empty started and finished tracklist tabs for future implementation.

---
+++ Frontend/src/features/user/activities-page/tabs/completed/completed.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'app-completed',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './completed.component.html',
+    styleUrls: ['./completed.component.css'],
+})
+export class CompletedComponent {}
---
+++ Frontend/src/features/user/activities-page/tabs/started/started.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'app-started',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './started.component.html',
+    styleUrls: ['./started.component.css'],
+})
+export class StartedComponent {}
