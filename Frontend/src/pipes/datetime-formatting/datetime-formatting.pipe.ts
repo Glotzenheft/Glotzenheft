9e3ebb01bb9bf92551678521b4fa73b79c4d5bca
Enuresh <138758218+Enuresh@users.noreply.github.com>
2026-01-21 13:22:01 +0100

    Add DateTimeFormattingPipe to format dates in 'de-DE' locale.

---
+++ Frontend/src/pipes/datetime-formatting/datetime-formatting.pipe.ts
@@ -0,0 +1,33 @@
+/*
+This file is part of Glotzenheft.
+
+Glotzenheft is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Glotzenheft is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'dateTimeFormatting',
+})
+export class DateTimeFormattingPipe implements PipeTransform {
+    transform(date: string): string {
+        return new Date(date).toLocaleString('de-DE', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+        });
+    }
+}
