3b93eaa046cf459980a23c8d3005f3f37da2989b
Enuresh <138758218+Enuresh@users.noreply.github.com>
2026-02-28 23:58:45 +0100

    Refactor `TracklistResponseDto`: make class readonly and add constructor with typed properties.

--- Backend/src/Model/Response/Tracklist/TracklistResponseDto.php
+++ Backend/src/Model/Response/Tracklist/TracklistResponseDto.php
@@ -8,7 +8,29 @@ use App\Model\Response\Media\MediaLightDetailResponseDto;
 use App\Model\Response\Tracklist\TracklistSeason\TracklistSeasonDetailDataDto;
 use App\Model\Response\TracklistTag\TracklistTagResponseDto;
 
-class TracklistResponseDto
+readonly class TracklistResponseDto
 {
-
+    public function __construct(
+        public int    $id,
+        public string $tracklistName,
+        public string $createdAt,
+        public string $updatedAt,
+        public string $status,
+        public int    $rating,
+        public bool   $isRewatching,
+        public string $startDate,
+        public string $finishDate,
+        /**
+         * @var TracklistSeasonDetailDataDto[]
+         */
+        public array  $tracklistSeasons,
+        /**
+         * @var TracklistTagResponseDto[]
+         */
+        public array  $tags,
+        /**
+         * @var MediaLightDetailResponseDto[]
+         */
+        public array  $media,
+    ){}
 }
