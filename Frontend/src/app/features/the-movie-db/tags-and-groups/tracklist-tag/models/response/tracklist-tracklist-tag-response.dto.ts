d534fd8dd276088ce0f53d4146b8fc7fa98061eb
Enuresh <138758218+Enuresh@users.noreply.github.com>
2026-05-07 14:11:32 +0200

    feat(tracklist tag): add `tracklistStatus` and `tracklistRating` fields to response DTO

--- Frontend/src/app/features/the-movie-db/tags-and-groups/tracklist-tag/models/response/tracklist-tracklist-tag-response.dto.ts
+++ Frontend/src/app/features/the-movie-db/tags-and-groups/tracklist-tag/models/response/tracklist-tracklist-tag-response.dto.ts
@@ -25,4 +25,6 @@ export interface TracklistTracklistTagResponseDto {
     mediaOriginalName: string;
     mediaPosterPath: string;
     mediaType: MediaType;
+    tracklistStatus: string;
+    tracklistRating: number | null;
 }
